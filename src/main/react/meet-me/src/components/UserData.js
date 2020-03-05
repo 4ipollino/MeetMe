3dfac3dc33b8ca55c165a50d0a88b664159e3557
chipollino <kirill.lucky.405@gmail.com>
2020-03-05 14:42:23 +0300

    Renamed HeaderNav to UserData, header is now in MainView

---
+++ src/main/react/meet-me/src/components/UserData.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Image} from "grommet";
+
+class UserData extends Component
+{
+    render() {
+        return(
+            <div>
+                <Image src='../logo.svg'/>
+            </div>
+        );
+    }
+}
+
+export default UserData;
